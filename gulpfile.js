87849a28663ddb3032a8461acbaba64fd221a00a
Seunghyun Choi <sh4682.choi@samsung.com>
2019-05-21 15:58:09 +0900

    Update version 1.0.1-beta for publish

--- gulpfile.js
+++ gulpfile.js
@@ -4,8 +4,5 @@ const gulp = require('gulp');
 const boilerplate = require('appium-gulp-plugins').boilerplate.use(gulp);
 
 boilerplate({
-  build: 'appium-sdb',
-  e2eTest: {
-    files: '${testDir}/functional/*-e2e-specs.js',
-  },
+  build: 'appium-sdb'
 });
